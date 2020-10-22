6775882b7026b7f6a94197256a7cd2abd480b20d
Filippo Tessarotto <zoeslam@gmail.com>
2020-10-22 11:16:52 +0200

    Tests ok on 7.4

--- src/FoldersPlugin.php
+++ src/FoldersPlugin.php
@@ -10,9 +10,8 @@ use Composer\Script\ScriptEvents;
 
 final class FoldersPlugin implements PluginInterface, EventSubscriberInterface
 {
-    private $composer;
-
-    private $io;
+    private Composer $composer;
+    private IOInterface $io;
 
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -27,7 +26,15 @@ final class FoldersPlugin implements PluginInterface, EventSubscriberInterface
         );
     }
 
-    public function createAndCleanFolders()
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function createAndCleanFolders(): void
     {
         $this->io->write('> ' . __METHOD__);
 
@@ -37,50 +44,63 @@ final class FoldersPlugin implements PluginInterface, EventSubscriberInterface
         $extra = $this->composer->getPackage()->getExtra();
         $foldersExtra = $extra['folders-plugin'];
 
-        foreach ($foldersExtra['create'] as $dir => $mode) {
-            $path = $rootDir . DIRECTORY_SEPARATOR . $dir;
+        if (isset($foldersExtra['create'])) {
+            foreach ($foldersExtra['create'] as $dir => $mode) {
+                $path = $rootDir . DIRECTORY_SEPARATOR . $dir;
 
-            if ($this->skipPath($path)) {
-                continue;
+                if ($this->isLink($path)) {
+                    continue;
+                }
+
+                $this->assertPathEqualsRealpath(dirname($path));
+
+                $mode = octdec($mode);
+                if (! is_dir($path)) {
+                    mkdir($path, $mode, true);
+                }
+                if (fileowner($path) === posix_getuid()) {
+                    chmod($path, $mode);
+                }
+
+                $this->io->write(sprintf(
+                    'Created folder (<comment>0%o > 0%o</comment>) <info>./%s</info>',
+                    $mode,
+                    fileperms($path),
+                    substr($path, strlen($rootDir) + 1)
+                ));
             }
-
-            $this->checkPath(dirname($path));
-
-            $mode = octdec($mode);
-            if (! is_dir($path)) {
-                mkdir($path, $mode, true);
-            }
-            if (fileowner($path) === posix_getuid()) {
-                chmod($path, $mode);
-            }
-
-            $this->io->write(sprintf('Created folder (<comment>0%o > 0%o</comment>) <info>./%s</info>', $mode, fileperms($path), substr($path, strlen($rootDir) + 1)));
         }
 
-        foreach ($foldersExtra['clean'] as $dir => $glob) {
-            $path = $rootDir . DIRECTORY_SEPARATOR . $dir;
+        if (isset($foldersExtra['clean'])) {
+            foreach ($foldersExtra['clean'] as $dir => $glob) {
+                $path = $rootDir . DIRECTORY_SEPARATOR . $dir;
 
-            if ($this->skipPath($path)) {
-                continue;
+                if ($this->isLink($path)) {
+                    continue;
+                }
+
+                $this->assertPathEqualsRealpath($path);
+                if (strpos($glob, DIRECTORY_SEPARATOR) !== false) {
+                    throw new \InvalidArgumentException(sprintf('No relative path allowed in glob: "%s" => "%s"', $dir, $glob));
+                }
+
+                $path .= DIRECTORY_SEPARATOR . $glob;
+
+                shell_exec('rm --force --recursive ' . $path);
+
+                $this->io->write(sprintf('Cleaned folder <info>./%s</info>', substr($path, strlen($rootDir) + 1)));
             }
-
-            $this->checkPath($path);
-            if (strpos($glob, DIRECTORY_SEPARATOR) !== false) {
-                throw new \InvalidArgumentException(sprintf('No relative path allowed in glob: "%s" => "%s"', $dir, $glob));
-            }
-
-            $path .= DIRECTORY_SEPARATOR . $glob;
-
-            shell_exec('rm --force --recursive ' . $path);
-
-            $this->io->write(sprintf('Cleaned folder <info>./%s</info>', substr($path, strlen($rootDir) + 1)));
         }
     }
 
-    private function skipPath($path)
+    private function isLink(string $path): bool
     {
         if (is_link($path)) {
-            $this->io->write(sprintf('<comment>Symbolic link</comment> untouched: <info>%s</info> -> <info>%s</info>', $path, realpath($path)));
+            $this->io->write(sprintf(
+                '<comment>Symbolic link</comment> untouched: <info>%s</info> -> <info>%s</info>',
+                $path,
+                realpath($path)
+            ));
 
             return true;
         }
@@ -88,7 +108,7 @@ final class FoldersPlugin implements PluginInterface, EventSubscriberInterface
         return false;
     }
 
-    private function checkPath($path)
+    private function assertPathEqualsRealpath(string $path): void
     {
         if ($path !== realpath($path)) {
             throw new \InvalidArgumentException(sprintf('No relative path allowed: "%s" !== "%s"', $path, realpath($path)));
---
+++ tests/FoldersPluginTest.php
@@ -0,0 +1,177 @@
+<?php
+
+
+namespace SlamTest\Composer\Folders;
+
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\Factory;
+use Composer\IO\IOInterface;
+use Composer\Package\RootPackageInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Slam\Composer\Folders\FoldersPlugin;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @covers \Slam\Composer\Folders\FoldersPlugin
+ */
+final class FoldersPluginTest extends TestCase
+{
+    private string $workingDir;
+    /**
+     * @var IOInterface&MockObject
+     */
+    private $io;
+    private FoldersPlugin $plugin;
+
+    protected function setUp(): void
+    {
+        $this->workingDir = __DIR__ . '/TmpFolder';
+        (new Filesystem())->remove(glob($this->workingDir . '/*'));
+
+        $this->io = $this->createMock(IOInterface::class);
+        $this->plugin = new FoldersPlugin();
+    }
+
+    public function testHasSubscribedEvent(): void
+    {
+        self::assertNotEmpty(FoldersPlugin::getSubscribedEvents());
+
+        $this->plugin->deactivate(new Composer(), $this->io);
+        $this->plugin->uninstall(new Composer(), $this->io);
+    }
+
+    public function testCreateFolders(): void
+    {
+        $randomFolder = uniqid('data_');
+        $this->activatePluginFromJsonString(sprintf(<<<'JSON'
+{
+    "extra": {
+        "folders-plugin": {
+            "create": {
+                "data":     "0770",
+                "data/%s":  "0500",
+                "tmp":      "0777"
+            }
+        }
+    }
+}
+JSON
+        , $randomFolder));
+
+        $this->plugin->createAndCleanFolders();
+
+        self::assertDirectoryExists($this->workingDir . '/data');
+        self::assertDirectoryIsWritable($this->workingDir . '/data');
+        self::assertDirectoryExists($this->workingDir . '/data/' . $randomFolder);
+        self::assertDirectoryIsNotWritable($this->workingDir . '/data/' . $randomFolder);
+        self::assertDirectoryExists($this->workingDir . '/tmp');
+    }
+
+    public function testCleanFolders(): void
+    {
+        (new Filesystem())->mkdir($this->workingDir . '/tmp');
+        (new Filesystem())->touch($fileToKeep = $this->workingDir . uniqid('/tmp/to_keep_'));
+        (new Filesystem())->touch($fileToRemove = $this->workingDir . uniqid('/tmp/to_remove_'));
+
+        $this->activatePluginFromJsonString(sprintf(<<<'JSON'
+{
+    "extra": {
+        "folders-plugin": {
+            "clean": {
+                "tmp": "to_remove_*"
+            }
+        }
+    }
+}
+JSON
+        ));
+
+        $this->plugin->createAndCleanFolders();
+
+        self::assertFileExists($fileToKeep);
+        self::assertFileDoesNotExist($fileToRemove);
+    }
+
+    public function testSkipLinks(): void
+    {
+        (new Filesystem())->mkdir($original = $this->workingDir . '/real');
+        (new Filesystem())->symlink($original, $symlink = $this->workingDir . '/link');
+        (new Filesystem())->touch($fileToKeep = $symlink . uniqid('/to_remove_'));
+
+        $this->activatePluginFromJsonString(sprintf(<<<'JSON'
+{
+    "extra": {
+        "folders-plugin": {
+            "create": {
+                "%1$s": "0500"
+            },
+            "clean": {
+                "%1$s": "to_remove_*"
+            }
+        }
+    }
+}
+JSON
+        , basename($symlink)));
+
+        $this->plugin->createAndCleanFolders();
+
+        self::assertDirectoryIsWritable($original);
+        self::assertFileExists($fileToKeep);
+    }
+
+    public function testForbidNonRealpaths(): void
+    {
+        $this->activatePluginFromJsonString(<<<'JSON'
+{
+    "extra": {
+        "folders-plugin": {
+            "create": {
+                "data/../tmp": "0700"
+            }
+        }
+    }
+}
+JSON
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $this->plugin->createAndCleanFolders();
+    }
+
+    public function testForbidPathsInGlobClean(): void
+    {
+        (new Filesystem())->mkdir($this->workingDir . '/tmp');
+
+        $this->activatePluginFromJsonString(<<<'JSON'
+{
+    "extra": {
+        "folders-plugin": {
+            "clean": {
+                "tmp": "/*"
+            }
+        }
+    }
+}
+JSON
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $this->plugin->createAndCleanFolders();
+    }
+
+    private function activatePluginFromJsonString(string $json): void
+    {
+        $jsonFile = $this->workingDir . '/composer.json';
+        (new Filesystem())->dumpFile($jsonFile, $json);
+
+        $composer = (new Factory)->createComposer($this->io, $jsonFile, false, $this->workingDir);
+
+        $this->plugin->activate($composer, $this->io);
+    }
+}
